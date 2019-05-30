4bb9d57e30373bd9c1f7c4e7341e7013a0ab8843
dli24 <dli24@buffalo.edu>
2019-05-29 22:09:19 -0700

    initializing

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+#from hike.models import Hike, Comments, HikeGroup
 
 # Create your models here.
+
+class Profile(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
+  bio = models.TextField(max_length=500)
+  picture = models.TextField()
+
+  def __str__(self):
+    return self.user.username
+
